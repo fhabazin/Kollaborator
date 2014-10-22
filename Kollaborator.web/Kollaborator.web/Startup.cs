34a73a16b5bb09c21803176351a378efc81f0742
fhabazin <fhabazin@gmail.com>
2014-10-22 15:43:04 +0200

    initial commit

---
+++ Kollaborator.web/Kollaborator.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kollaborator.web.Startup))]
+namespace Kollaborator.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
